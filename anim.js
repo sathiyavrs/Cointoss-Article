0d4f73a7bd0d76cce54d8fbdd4fb7a8343d54e65
Suyash458 <suyash.behera458@gmail.com>
2020-08-30 17:27:48 +0530

    add quadrant labels

--- anim.js
+++ anim.js
@@ -1,10 +1,14 @@
+
+const getBiasedCoinTosser = (bias) => () => {
+    return Math.random(0, 1) <= bias;
+}
+
 const getCanvasContext = () => {
     return document.getElementById('simulator1').getContext('2d');
 }
 
 const drawQuadrant = () => {
     ctx = getCanvasContext();
-
 
     ctx.beginPath();
     ctx.moveTo(ctx.canvas.width / 2, 0);
@@ -13,4 +17,9 @@ const drawQuadrant = () => {
     ctx.lineTo(ctx.canvas.width, ctx.canvas.height / 2);
 
     ctx.stroke();
+
+    ctx.fillText('HH', 10, 10)
+    ctx.fillText('HT', ctx.canvas.width - 20, 10)
+    ctx.fillText('TH', 10, ctx.canvas.height - 10)
+    ctx.fillText('TT', ctx.canvas.width - 20,  ctx.canvas.height - 10)
 }
